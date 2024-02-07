695f612458fe941b1e404a460bec75ea1a8588f9
Jacques Lucke <mail@jlucke.com>
2024-02-07 23:14:52 +0100

    progress

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -4,6 +4,34 @@ void main() {
   runApp(const MintInsiderPassApp());
 }
 
+class PassItemInfo {
+  String imagePath;
+  String title;
+
+  PassItemInfo({required this.imagePath, required this.title});
+}
+
+var passItems = [
+  PassItemInfo(
+      imagePath: "https://hackmd.io/_uploads/ryCnnbZia.png",
+      title: "Mess Master"),
+  PassItemInfo(
+      imagePath: "https://hackmd.io/_uploads/Sy2anWZoT.png",
+      title: "Försterdreieck"),
+  PassItemInfo(
+      imagePath: "https://hackmd.io/_uploads/SklA3Wbj6.png",
+      title: "Tricks und Knobeleien"),
+  PassItemInfo(
+      imagePath: "https://hackmd.io/_uploads/HJcAnZZop.png",
+      title: "Mathe-Magie"),
+  PassItemInfo(
+      imagePath: "https://hackmd.io/_uploads/BkxJ6WZja.png",
+      title: "Spielerisches Programmieren"),
+  PassItemInfo(
+      imagePath: "https://hackmd.io/_uploads/SyVJ6ZZsT.png",
+      title: "Calliope Mini"),
+];
+
 class MintInsiderPassApp extends StatelessWidget {
   const MintInsiderPassApp({super.key});
 
@@ -14,11 +42,15 @@ class MintInsiderPassApp extends StatelessWidget {
         appBar: AppBar(
             backgroundColor: Colors.green,
             title: const Text("MINT Insider Pass")),
-        body: Container(
-          child: GridView.count(
-            crossAxisCount: 2,
-            children: [PassItem(), PassItem(), PassItem()],
-          ),
+        body: GridView.builder(
+          itemBuilder: (context, index) {
+            if (index >= passItems.length) {
+              return null;
+            }
+            return PassItem(info: passItems[index]);
+          },
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2),
         ),
       ),
     );
@@ -26,12 +58,53 @@ class MintInsiderPassApp extends StatelessWidget {
 }
 
 class PassItem extends StatelessWidget {
+  final PassItemInfo info;
+
   const PassItem({
     super.key,
+    required this.info,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Text("1");
+    return GestureDetector(
+      child: Hero(
+        tag: info.imagePath,
+        child: Image.network(info.imagePath),
+      ),
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: ((context) => PassItemRoute(
+                      info: info,
+                    ))));
+      },
+    );
+  }
+}
+
+class PassItemRoute extends StatelessWidget {
+  final PassItemInfo info;
+
+  const PassItemRoute({super.key, required this.info});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Element"),
+        backgroundColor: Colors.green,
+      ),
+      body: Column(
+        children: [
+          Hero(
+            tag: info.imagePath,
+            child: Image.network(info.imagePath),
+          ),
+          Center(child: Text(info.title)),
+        ],
+      ),
+    );
   }
 }
